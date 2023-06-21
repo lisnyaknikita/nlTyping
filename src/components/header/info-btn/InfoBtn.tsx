1458f6fb64d9337e9b2df29da5062b7252887c02
nlisnyak <lisnyaknikita@gmail.com>
2023-06-21 12:51:21 +0300

    Added styles to header components

--- src/components/header/info-btn/InfoBtn.tsx
+++ src/components/header/info-btn/InfoBtn.tsx
@@ -1,14 +1,34 @@
-import { FC } from 'react';
+'use client';
+
+import { FC, MouseEventHandler, useState } from 'react';
 
 import classes from './InfoBtn.module.scss';
 
 import { FiInfo } from 'react-icons/fi';
 
+import clsx from 'clsx';
+
 const InfoBtn: FC = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const handleClick: MouseEventHandler<HTMLButtonElement> = (e) => {
+    setIsDropdownOpen((prev) => !prev);
+  };
+
   return (
-    <button className={classes.infoBtn}>
-      <FiInfo />
-    </button>
+    <>
+      <button type='button' className={classes.infoBtn} onClick={handleClick}>
+        <FiInfo />
+      </button>
+      <div className={clsx(classes.dropdown, isDropdownOpen && 'open')}>
+        <p>
+          GitHub:
+          <a href='https://github.com/lisnyaknikita/nlTyping' target='_blank'>
+            https://github.com/lisnyaknikita/nlTyping
+          </a>
+        </p>
+      </div>
+    </>
   );
 };
 
