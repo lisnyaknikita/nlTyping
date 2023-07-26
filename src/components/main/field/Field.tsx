dc4f68c10aedfcb2fdb242518c070c3742e532b3
nlisnyak <lisnyaknikita@gmail.com>
2023-07-26 17:43:49 +0300

    implemented typing field wrapper

--- src/components/main/field/Field.tsx
+++ src/components/main/field/Field.tsx
@@ -3,7 +3,11 @@ import { FC } from 'react';
 import classes from './Field.module.scss';
 
 const Field: FC = () => {
-  return <div>Field</div>;
+  return (
+    <div className={classes.field}>
+      
+    </div>
+  );
 };
 
 export default Field;
