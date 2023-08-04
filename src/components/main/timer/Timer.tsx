00fddec72f853d7ad7dc5ac6c24e18e9e8a485cb
nlisnyak <lisnyaknikita@gmail.com>
2023-08-04 17:30:40 +0300

    implemented timer

--- src/components/main/timer/Timer.tsx
+++ src/components/main/timer/Timer.tsx
@@ -6,18 +6,19 @@ import classes from './Timer.module.scss';
 const Timer: FC = () => {
   const [seconds, setSeconds] = useState(5);
 
-  let typingTimer: NodeJS.Timeout;
-
   useEffect(() => {
-    typingTimer = setInterval(() => {
-      if (seconds === 0) {
-        clearInterval(typingTimer);
-      } else {
-        setSeconds((prevSeconds) => prevSeconds - 1);
-      }
+    const interval = setInterval(() => {
+      setSeconds((prevSecond) => {
+        if (prevSecond === 0) {
+          clearInterval(interval);
+        }
+        return prevSecond > 0 ? prevSecond - 1 : prevSecond;
+      });
     }, 1000);
 
-    return () => clearInterval(typingTimer);
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
 
   return <div className={classes.timer}>{seconds}s</div>;
