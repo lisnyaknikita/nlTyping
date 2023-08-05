328e44b8ed755fe90a203f9cca65be88f12411c5
nlisnyak <lisnyaknikita@gmail.com>
2023-08-05 19:20:01 +0300

    added 3 json with random words and displayed it

--- src/components/main/field/Field.tsx
+++ src/components/main/field/Field.tsx
@@ -1,26 +1,54 @@
 'use client';
-import { FC, useContext } from 'react';
+import { FC, FormEvent, useContext, useEffect, useRef, useState } from 'react';
 
 import classes from './Field.module.scss';
 
-import { faker } from '@faker-js/faker';
+import data from '@/languages/english.json';
 
 import clsx from 'clsx';
 
 import { ThemeContext } from '@/providers/ThemeContext';
 
 const Field: FC = () => {
+  const [inputValue, setInputValue] = useState<string>('');
+  const [englishWords, setEnglishWords] = useState<string>('');
+
   const { darkMode } = useContext(ThemeContext);
 
-  const englishWords = faker.random.words(30);
-  // const ukrainianWords = faker.random.words(30);
-  // const russianWords = faker.random.words(30);
-  //TODO: add ukrainian and russian languages
+  const iteratorRef = useRef(0);
+
+  const fromIndex = Math.round(Math.random() * (957 - 0) + 0);
+
+  useEffect(() => {
+    const english = data.words.slice(fromIndex, fromIndex + 40).toString();
+    setEnglishWords(english);
+  }, []);
+
+  const handleTyping = (e: FormEvent<HTMLInputElement>) => {
+    setInputValue(e.currentTarget.value);
+
+    const generatedWords = englishWords;
+
+    console.log(
+      e.currentTarget.value === generatedWords.charAt(iteratorRef.current)
+    );
+    iteratorRef.current++;
+    console.log(iteratorRef.current);
+  };
 
   return (
-    <div className={clsx(classes.field, !darkMode && 'light')}>
-      <div className={classes.wordsBox}>{englishWords}</div>
-    </div>
+    <>
+      <div className={clsx(classes.field, !darkMode && 'light')}>
+        <div className={classes.wordsBox}>{englishWords}</div>
+      </div>
+      <input
+        className={classes.input}
+        style={{ textAlign: 'center', marginLeft: '45%' }}
+        type='text'
+        value={inputValue}
+        onChange={handleTyping}
+      />
+    </>
   );
 };
 
